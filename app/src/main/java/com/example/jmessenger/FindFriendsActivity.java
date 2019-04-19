a68823455232ed2cdfc1801d2fb50ad12f0be8fd
Jaredlol <thisisfordiscord89@gmail.com>
2019-04-19 12:46:03 -0400

    Added ProfileActivity Class to get user ids

--- app/src/main/java/com/example/jmessenger/FindFriendsActivity.java
+++ app/src/main/java/com/example/jmessenger/FindFriendsActivity.java
@@ -1,10 +1,24 @@
 package com.example.jmessenger;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class FindFriendsActivity extends AppCompatActivity
@@ -12,6 +26,7 @@ public class FindFriendsActivity extends AppCompatActivity
 
     private Toolbar mToolbar;
     private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UserRef;
 
 
     @Override
@@ -19,6 +34,10 @@ public class FindFriendsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
 
         FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.main_find_friends_recycler_list);
         FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
@@ -28,6 +47,67 @@ public class FindFriendsActivity extends AppCompatActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
+    }
 
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(UserRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendsViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, FindFriendsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendsViewHolder holder, final int position, @NonNull Contacts model)
+                    {
+                        holder.userName.setText(model.getName());
+                        holder.userStatus.setText(model.getStatus());
+                        Picasso.get().load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v)
+                            {
+                                String visit_user_id = getRef(position).getKey();
+
+                                Intent profileIntent = new Intent(FindFriendsActivity.this, ProfileActivity.class);
+                                profileIntent.putExtra("visit_user_id", visit_user_id);
+                                startActivity(profileIntent);
+                            }
+                        });
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
+                        FindFriendsViewHolder viewHolder = new FindFriendsViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+
+    }
+
+    public static class FindFriendsViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+
+        public FindFriendsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+            profileImage = itemView.findViewById(R.id.user_profile_image);
+        }
     }
 }
